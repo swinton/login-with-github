96289d0b7f66e15b4ecd9ab78f1e914c5c7e9011
Glitch (hello-express) <none>
2019-04-12 21:14:32 +0000

    👋🍬 Checkpoint ./server.js:299944/1649 ./package.json:299944/1552

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 // server.js
 // where your node app starts
 
-// init project
+// for HTTP request
+const axios = require('axios');
+
+// for web framework
 const express = require('express');
 const app = express();
+
+// for session storage
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
+
+// for template rendering
 const nunjucks = require('nunjucks');
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+// for GitHub API
+const Octokit = require('@octokit/rest');
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -31,13 +38,32 @@ app.get('/', function(request, response) {
     request.session.views = 1;
   }
   
+  // for OAuth authorzations
+  const code = request.query.code;
+  const state = request.query.state;
+  if (code && state) {
+    // exchange for token
+    axios.post('https://github.com/login/oauth/access_token', {
+      client_id: process.env.CLIENT_ID,
+      client_secret: process.env.CLIENT_SECRET,
+      code: code,
+      state: 
+    });
+  } 
   
+  if (request.session.token) {
+    // instantiate client with token
+  }
+
   // render and send the page
   response.send(nunjucks.render(
     'views/index.html',
     {
       'state': request.query.state || false, 
-      'title':process.env.TITLE }
+      'title': process.env.TITLE,
+      'installation_id': request.query.installation_id,
+      'setup_action': request.query.setup_action,
+    }
   ));
 });
 
