4fa35e640d3ee54fac9203319699cac43318a907
Steve Winton <stevewinton@gmail.com>
2019-11-01 12:50:14 -0500

    Add script for starting server

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,11 @@ RUN apt-get update -y && apt-get upgrade -y
 RUN mkdir /app
 WORKDIR /app
 
-COPY app/ /app
-RUN npm install --only=production
+COPY package*.json ./
+RUN npm ci --only=production
+
+# Bundle app source
+COPY . .
 
 EXPOSE 8000
 ENTRYPOINT ["node", "/app/server.js"]
--- script/server.sh
+++ script/server.sh
@@ -2,17 +2,21 @@
 
 cd $( dirname "$0" )/..
 
+port=${1:-49160}
+
 # Stop the container, don't wait to kill it
-docker stop --time 0 login-with-github
+docker stop --time 0 login-with-github > /dev/null 2>&1
 
 # Remove the container
-docker rm login-with-github
+docker rm login-with-github > /dev/null > /dev/null 2>&1
+
+echo "Running. To stop:\ndocker stop --time 0 login-with-github"
 
 # Run the container
-docker run --name login-with-github \
-  --interactive \
+container=$( docker run --name login-with-github \
+  -p $port:8000 \
   --detach \
   --env-file ./.env \
-  login-with-github
+  login-with-github )
 
-echo "🚀 Running. To stop:\ndocker stop --time 0 login-with-github"
+docker logs --follow $container
